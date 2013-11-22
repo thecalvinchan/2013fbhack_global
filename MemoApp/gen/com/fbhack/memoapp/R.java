68c48603619149471c8febc31d2750f3f5493576
Daniel Duan <danielduan88@yahoo.com>
2013-11-21 21:12:01 -0800

    identify trigger

--- MemoApp/gen/com/fbhack/memoapp/R.java
+++ MemoApp/gen/com/fbhack/memoapp/R.java
@@ -36,7 +36,7 @@ public final class R {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060002;
-        public static final int remember_voice_prompt=0x7f060004;
+        public static final int identify_voice_trigger=0x7f060004;
         public static final int remember_voice_trigger=0x7f060003;
     }
     public static final class style {
@@ -68,6 +68,7 @@ public final class R {
         public static final int AppTheme=0x7f070001;
     }
     public static final class xml {
-        public static final int remember_voice_trigger=0x7f040000;
+        public static final int identify_voice_trigger=0x7f040000;
+        public static final int remember_voice_trigger=0x7f040001;
     }
 }
